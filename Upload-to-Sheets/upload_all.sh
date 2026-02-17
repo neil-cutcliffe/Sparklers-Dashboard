ae9dc8e9d10ce5e1e2d19eef24ec14bd29e5608f
EC2 Default User <ec2-user@ip-10-0-37-101.ec2.internal>
2026-02-17 03:50:44 +0000

    Upload the Klaviyo data too.

--- Upload-to-Sheets/upload_all.sh
+++ Upload-to-Sheets/upload_all.sh
@@ -3,25 +3,29 @@
 # The IDs of the spreadsheets
 SPARKLER_DATA_WORDPRESS="1uS5wqj7nrVmZlSHA06kEK6QgGg2A0RYreT8SjCI2ABk"
 ALL_SPARKLERS="All Sparklers"
-ALL_SPARKLERS_ROWS=6
 
 SPARKLER_DATA_LOGINS="1uS5wqj7nrVmZlSHA06kEK6QgGg2A0RYreT8SjCI2ABk"
 LOGGED_IN="Logged In"
-LOGGED_IN_ROWS=3
 
 SPARKLER_DATA_BUN_CREDS="1T0kWyi920M0M3I5qioSefzYb00qLUXVXtbE3muZ_8C8"
 BUNDLE_CREDITS="Bundle Credits"
-BUNDLE_CREDITS_ROWS=4
 
 SPARKLER_GOLD_STARS="1Vg3lmhnI_2F-aoqpJxk1LqIMs_0F5NseaFVeG8u4p18"
 GOLD_STARS="Gold Stars"
-GOLD_STARS_ROWS=1
+
+SPARKLER_KLAVIYO="11IhWEVIPbM23meiS1HbNUgkc0lUCVjXRicYqS_MuL2s"
+NEWSLETTER_1="1 Week"
+
+SPARKLER_KLAVIYO="11IhWEVIPbM23meiS1HbNUgkc0lUCVjXRicYqS_MuL2s"
+NEWSLETTER_2="2 Weeks"
+
+SPARKLER_KLAVIYO="11IhWEVIPbM23meiS1HbNUgkc0lUCVjXRicYqS_MuL2s"
+NEWSLETTER_4="4 Weeks"
+
+SPARKLER_KLAVIYO="11IhWEVIPbM23meiS1HbNUgkc0lUCVjXRicYqS_MuL2s"
+NEWSLETTER_6="6 Weeks"
 
 source venv/bin/activate
-
-
-#Jan 29th - Row param no longer needed. It is grabbed dynamically from csv file in python code
-
 
 
 #echo
@@ -30,11 +34,11 @@ source venv/bin/activate
 #    "${ALL_SPARKLERS}"                  \
 #    "../WooCommerce/sparkler-subscriptions.csv"
 
-echo
-python3 upload_to_sheets.py             \
-    "${SPARKLER_DATA_LOGINS}"           \
-    "${LOGGED_IN}"                      \
-    "../WordPress/sparkler-logins.csv"
+#echo
+#python3 upload_to_sheets.py             \
+#    "${SPARKLER_DATA_LOGINS}"           \
+#    "${LOGGED_IN}"                      \
+#    "../WordPress/sparkler-logins.csv"
 
 
 #echo
@@ -48,4 +52,31 @@ python3 upload_to_sheets.py             \
 #    "${SPARKLER_GOLD_STARS}"        \
 #    "${GOLD_STARS}"                 \
 #    "../Wishlist/gold-stars.csv"
+
+#echo
+#python3 upload_to_sheets.py             \
+#    "${SPARKLER_KLAVIYO}"               \
+#    "${NEWSLETTER_1}"                   \
+#    "../Klaviyo/TSC-Newsletter-Opens-1.csv"
+
+
+echo
+python3 upload_to_sheets.py             \
+    "${SPARKLER_KLAVIYO}"               \
+    "${NEWSLETTER_2}"                   \
+    "../Klaviyo/TSC-Newsletter-Opens-2.csv"
+
+
+echo
+python3 upload_to_sheets.py             \
+    "${SPARKLER_KLAVIYO}"               \
+    "${NEWSLETTER_4}"                   \
+    "../Klaviyo/TSC-Newsletter-Opens-4.csv"
+
+
+#echo
+#python3 upload_to_sheets.py             \
+#    "${SPARKLER_KLAVIYO}"               \
+#    "${NEWSLETTER_6}"                   \
+#    "../Klaviyo/TSC-Newsletter-Opens-6.csv"
 
